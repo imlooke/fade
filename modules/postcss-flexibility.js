30b37dad3266e63888287c4a9fd645bc394b2c9d
lwx12525 <lwx12525@qq.com>
2021-06-03 16:38:42 +0800

    init

---
+++ modules/postcss-flexibility.js
@@ -0,0 +1,24 @@
+const postcssFlexibility = () => ({
+  postcssPlugin: "postcss-flexibility",
+  Once(root) {
+    root.walkRules((rule) => {
+      const isDisabled = rule.nodes.some(
+        ({ prop, text = "" }) =>
+          prop === "-js-display" || text.endsWith("flexibility-disable")
+      );
+
+      if (!isDisabled) {
+        rule.walkDecls("display", (decl) => {
+          const { value } = decl;
+          if (/^flex$|^inline-flex$/.test(value)) {
+            decl.cloneBefore({ prop: "-js-display" });
+          }
+        });
+      }
+    });
+  },
+});
+
+postcssFlexibility.postcss = true;
+
+module.exports = postcssFlexibility;
